2293631684ce8c5f700d441b3c55bd02bd6aa780
Nappster2208 <piamaulanaibrahim@gmail.com>
2023-11-25 21:11:10 +0700

    feat: Add fallback metadata to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="">
-      <h1 className="">Hello World</h1>
+      <h1 className="font-body">Hello World</h1>
     </main>
   )
 }
