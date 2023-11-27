a83f21c0d26ed00d8a416baf67354fef0aef242d
Nappster2208 <piamaulanaibrahim@gmail.com>
2023-11-27 20:23:34 +0700

    feat: Add gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import clsx from 'clsx'
-import { Nunito, Nunito_Sans } from 'next/font/google'
-import { createClient } from '@/prismicio'
+import "./globals.css";
+import type { Metadata } from "next";
+import clsx from "clsx";
+import { Nunito, Nunito_Sans } from "next/font/google";
+import { createClient } from "@/prismicio";
 
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const nunito = Nunito({
-  subsets: ['latin'],
-  variable: '--font-nunito',
-  display: 'swap',
-})
+  subsets: ["latin"],
+  variable: "--font-nunito",
+  display: "swap",
+});
 
 const nunitoSans = Nunito_Sans({
-  subsets: ['latin'],
-  variable: '--font-nunito-sans',
-  display: 'swap',
-})
+  subsets: ["latin"],
+  variable: "--font-nunito-sans",
+  display: "swap",
+});
 
 // export const metadata: Metadata = {
 //   title: 'Create Next App',
@@ -25,24 +25,24 @@ const nunitoSans = Nunito_Sans({
 // }
 
 export async function generateMetadata(): Promise<Metadata> {
-  
   const client = createClient();
 
   const settings = await client.getSingle("settings");
 
   return {
     title: settings.data.site_title || "Flowrise fallback",
-    description: settings.data.meta_description || "Flowrise is the relaxing app for you.",
+    description:
+      settings.data.meta_description || "Flowrise is the relaxing app for you.",
     openGraph: {
       images: [settings.data.og_image.url || ""],
     },
-  }
+  };
 }
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
@@ -50,7 +50,8 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50" />
       </body>
     </html>
-  )
+  );
 }
